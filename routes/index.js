cc0192a87779a4a6ec55a815709ef8bea5d4fe53
Kamil Kochanczyk <kochanczykkamil05@gmail.com>
2024-02-02 20:39:50 +0100

    Static pages (without sign up, log in and comments)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kamil' });
+function loadPage(res, titleArg, templateArg, styleArg, scriptArg, loggedInArg, loggedInUserArg) {
+    res.render('index', {
+        title: titleArg,
+        specificTemplate: templateArg,
+        specificStyle: styleArg,
+        specificScript: scriptArg,
+        loggedIn: loggedInArg,
+        loggedInUser: loggedInUserArg
+    });
+}
+
+router.get('/', (req, res) => {
+    loadPage(res, 'Home', 'home', 'home', null, false, null);
+});
+
+router.get('/founders', (req, res) => {
+    loadPage(res, 'Founders', 'founders', 'founders', null, false, null);
+});
+
+router.get('/albums', (req, res) => {
+    loadPage(res, 'Albums', 'albums', 'albums', 'albums', false, null);
+});
+
+router.get('/news', (req, res) => {
+    loadPage(res, 'News', 'news', 'news', null, false, null);
 });
 
 module.exports = router;
